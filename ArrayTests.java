4ca92329a9659d264f914b26486de30a604e591a
kayden-yan <122576038+kayden-yan@users.noreply.github.com>
2023-01-26 15:09:15 -0800

    array method update

--- ArrayTests.java
+++ ArrayTests.java
@@ -4,15 +4,34 @@ import org.junit.*;
 public class ArrayTests {
 	@Test 
 	public void testReverseInPlace() {
-    int[] input1 = { 3 };
+    int[] input1 = {1,2,3};
     ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
+    assertArrayEquals(new int[]{3,2,1}, input1);
 	}
 
+  @Test
+  public void testReverseInPlace2() {
+    int[] input1 = {2,1};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{1,2}, input1);
+	}
+
+  @Test
+  public void testReverseInPlace3() {
+    int[] input1 = {1,2,3,4,5,6};
+    ArrayExamples.reverseInPlace(input1);
+    assertArrayEquals(new int[]{6,5,4,3,2,1}, input1);
+	}
 
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReversed2() {
+    int[] input1 = {1,2,3};
+    assertArrayEquals(new int[]{3,2,1}, ArrayExamples.reversed(input1));
   }
 }
