b849e919caf0cda6c4d512c77dadc5fec2de6c36
kayden-yan <122576038+kayden-yan@users.noreply.github.com>
2023-02-13 18:19:37 -0800

    Update ArrayTests.java

--- ArrayTests.java
+++ ArrayTests.java
@@ -4,9 +4,9 @@ import org.junit.*;
 public class ArrayTests {
 	@Test 
 	public void testReverseInPlace() {
-    int[] input1 = {1,2,3};
+    int[] input1 = {1};
     ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{3,2,1}, input1);
+    assertArrayEquals(new int[]{1}, input1);
 	}
 
   @Test
